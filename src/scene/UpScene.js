ad373ad95ce13f03379469f49cb0353025d5d331
zidong.wang <zidong.wang@qunar.com>
2016-01-19 15:57:16 +0800

    finish

--- src/scene/UpScene.js
+++ src/scene/UpScene.js
@@ -1,11 +1,14 @@
 var UpLayer = qc.Layer.extend({
     bgSprite:null,
     bgLayer:null,
+    peopleLayer:null,
     winSize:null,
     midpos:null,
     init:function(){
         this.initData();
         this.initBg();
+        this.initPeople();
+        this.initListener();
     },
     initData:function(){
         this.winSize = qc.director.getWinSize();
@@ -17,6 +20,24 @@ var UpLayer = qc.Layer.extend({
         this.addChild(this.bgLayer);
         this.bgLayer.addChild(this.bgSprite);
         this.bgLayer.setPosition(this.midpos);
+
+        var label = qc.Label.create("举牌小人","微软雅黑","bold 40");
+        label.setColor(qc.color(255,51,204));
+        label.setPosition(qc.p(-80,300));
+        this.bgLayer.addChild(label);
+    },
+    initPeople:function(){
+        this.peopleLayer = ShowLayer.create();
+        this.addChild(this.peopleLayer);
+        this.peopleLayer.setScale(0.75);
+        this.peopleLayer.setPosition(this.midpos);
+    },
+    initListener:function(){
+        var _this = this;
+        $("#inText").on("input",function(){
+            var text = $(this).val();
+            _this.peopleLayer.add(text);
+        });
     }
 });
 
