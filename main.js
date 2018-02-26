07a0a2d11e205fc921c8fb519a26eee01ad9f2bd
wangzidong <wangzidong@meituan.com>
2018-02-26 16:11:10 +0800

    变成npm包发布

--- main.js
+++
@@ -1,7 +0,0 @@
-qc.game.onStart = function(){
-    LoaderScene.preload(start_resources,function(){
-        var myScene = new MyScene();
-        qc.director.runScene(myScene);
-    });
-};
-qc.game.run();
